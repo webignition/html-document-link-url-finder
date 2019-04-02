baeba8c55837fce29ac1b1c42bf505e60c67acd9
Jon Cram <jon@webignition.net>
2019-04-02 19:00:26 +0100

    Remove HtmlDocumentLinkUrlFinder::hasUrls()

--- src/HtmlDocumentLinkUrlFinder.php
+++ src/HtmlDocumentLinkUrlFinder.php
@@ -259,11 +259,6 @@ class HtmlDocumentLinkUrlFinder
         return false;
     }
 
-    public function hasUrls(): bool
-    {
-        return count($this->getUniqueUrls()) > 0;
-    }
-
     private function getElementsWithUrlAttributes(): array
     {
         if (empty($this->elementsWithUrlAttributes)) {
