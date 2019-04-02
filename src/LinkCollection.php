369063eacfc72d41e39c05a370294a2f48f5b7ff
Jon Cram <jon@webignition.net>
2019-04-02 15:47:29 +0100

    Add methods, test

--- src/LinkCollection.php
+++ src/LinkCollection.php
@@ -2,12 +2,16 @@
 
 namespace webignition\HtmlDocumentLinkUrlFinder;
 
-class LinkCollection
+use webignition\Uri\Uri;
+
+class LinkCollection implements \Iterator
 {
     /**
      * @var Link[]
      */
     private $links;
+
+    private $iteratorPosition = 0;
 
     public function __construct(array $links = [])
     {
@@ -16,5 +20,67 @@ class LinkCollection
                 $this->links[] = $link;
             }
         }
+
+        $this->iteratorPosition = 0;
+    }
+
+    public function rewind()
+    {
+        $this->iteratorPosition = 0;
+    }
+
+    public function current(): Link
+    {
+        return $this->links[$this->iteratorPosition];
+    }
+
+    public function key()
+    {
+        return $this->iteratorPosition;
+    }
+
+    public function next()
+    {
+        ++$this->iteratorPosition;
+    }
+
+    public function valid()
+    {
+        return isset($this->links[$this->iteratorPosition]);
+    }
+
+    public function getUrls(): array
+    {
+        $urls = [];
+
+        foreach ($this->links as $link) {
+            $urls[] = $link->getUrl();
+        }
+
+        return $urls;
+    }
+
+    public function getUniqueUrls(bool $ignoreFragment = false): array
+    {
+        $allUrls = $this->getUrls();
+        $uniqueUrls = [];
+
+        foreach ($allUrls as $url) {
+            if ($ignoreFragment) {
+                $url = $this->createUniquenessComparisonUrl($url);
+            }
+
+            $uniqueUrls[$url] = $url;
+        }
+
+        return array_values($uniqueUrls);
+    }
+
+    private function createUniquenessComparisonUrl(string $url): string
+    {
+        $uri = new Uri($url);
+        $uri = $uri->withFragment('');
+
+        return (string) $uri;
     }
 }
